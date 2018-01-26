179f7886e1b608ffd2cea39bde6f2e6998c88b76
Nic Travis <nptravis@yahoo.com>
2018-01-26 02:43:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ var body = document.getElementsByTagName('body')[0];
 function init() {
   var i = 0;
     body.addEventListener('keydown', function(e){
-      const key = parseInt(e.which || e.details);
+      const key = parseInt(e.which || e.detail);
       if (key === parseInt(code[i])){
   				i++;
   			} else{
